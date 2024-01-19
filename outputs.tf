2908e60f1d742b65733f6d2b383da56fb6aafdad
Tolu Onasanya <tolu.onasanya@liveramp.com>
2024-01-19 12:22:43 +0000

    output tenant data svc account id

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "tenant_data_access_email" {
   value = google_service_account.tenant_data_access.email
 }
+
+output "tenant_data_access_svc_account_id" {
+  value = google_service_account.tenant_data_access.id
+}
