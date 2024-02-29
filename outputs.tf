5437d749e411b83bb1155a53cb9bcfc4f8631d80
Prameeth Edirisinghe <prameeth.edirisinghe@liveramp.com>
2024-02-29 10:46:50 +0000

    Add descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "tenant_data_access_svc_account_name" {
-  value = google_service_account.tenant_data_access.name
+  value       = google_service_account.tenant_data_access.name
+  description = "The name of the service account that will be used to access the tenant data"
 }
 
 output "tenant_bigquery_dataset_name" {
-  value = google_bigquery_dataset.tenant_dataset.dataset_id
+  value       = google_bigquery_dataset.tenant_dataset.dataset_id
+  description = "The name of the BigQuery dataset that will be used to store the tenant data"
 }
 
 output "input_bucket_name" {
-  value = google_storage_bucket.tenant_input_bucket.name
+  value       = google_storage_bucket.tenant_input_bucket.name
+  description = "The name of the GCS bucket that will be used to store the input files"
 }
 
 output "build_bucket_name" {
-  value = google_storage_bucket.tenant_build_bucket.name
+  value       = google_storage_bucket.tenant_build_bucket.name
+  description = "The name of the GCS bucket that will be used to store the build files"
 }
 
 output "output_bucket_name" {
-  value = google_storage_bucket.tenant_output_bucket.name
+  value       = google_storage_bucket.tenant_output_bucket.name
+  description = "The name of the GCS bucket that will be used to store the output files"
 }
