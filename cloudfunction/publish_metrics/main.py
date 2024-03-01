f4d54f1f54cf67430c28b8f96b73cf18aca37913
Prameeth Edirisinghe <prameeth.edirisinghe@liveramp.com>
2024-03-01 12:10:31 +0000

    Update format

--- cloudfunction/publish_metrics/main.py
+++ cloudfunction/publish_metrics/main.py
@@ -1,18 +1,17 @@
 import base64
-import os
 import functions_framework
+import os
 
-from google.cloud import pubsub_v1
 from cloudevents.http import CloudEvent
+from google.cloud import pubsub_v1
 
-# Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
 def publish_metrics(cloud_event: CloudEvent) -> None:
     destination_project_id = os.getenv('TARGET_GOOGLE_CLOUD_PROJECT')
     destination_topic_name = os.getenv('TARGET_TOPIC_NAME')
 
     pubsub_message = base64.b64decode(cloud_event.data["message"]["data"]).decode()
-    attributes = cloud_event.data["attributes"]
+    attributes = cloud_event.data["message"]["attributes"]
 
     publisher = pubsub_v1.PublisherClient()
     destination_topic_path = f'projects/{destination_project_id}/topics/{destination_topic_name}'
