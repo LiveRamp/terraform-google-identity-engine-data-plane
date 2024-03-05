bd956423a72483c120cfc4edea526c41883aac52
Tolu Onasanya <tolu.onasanya@liveramp.com>
2024-03-05 15:40:54 +0000

    add flag to turn metric infra off

--- private.metrics.tf
+++ private.metrics.tf
@@ -1,17 +1,19 @@
 resource "random_id" "uuid" {
   byte_length = 6
+  count       = var.enable_metrics_infra ? 1 : 0
 }
 
 resource "google_pubsub_schema" "metric_schema" {
   project    = var.data_plane_project
-  name       = "metric-${random_id.uuid.hex}"
+  name       = "metric-${random_id.uuid[0].hex}"
   type       = "AVRO"
   definition = file("${path.module}/schema/metric-schema.json")
+  count      = var.enable_metrics_infra ? 1 : 0
 }
 
 resource "google_pubsub_topic" "metrics_topic" {
   project = var.data_plane_project
-  name    = lower("private.${var.installation_name}.metrics-${random_id.uuid.hex}")
+  name    = lower("private.${var.installation_name}.metrics-${random_id.uuid[0].hex}")
 
   labels = {
     installation_name = lower(var.installation_name)
@@ -21,9 +23,11 @@ resource "google_pubsub_topic" "metrics_topic" {
 
   depends_on = [google_pubsub_schema.metric_schema]
   schema_settings {
-    schema   = "projects/${var.data_plane_project}/schemas/${google_pubsub_schema.metric_schema.name}"
+    schema   = "projects/${var.data_plane_project}/schemas/${google_pubsub_schema.metric_schema[0].name}"
     encoding = "JSON"
   }
+
+  count = var.enable_metrics_infra ? 1 : 0
 }
 
 data "google_iam_policy" "metrics_publisher_subscriber" {
@@ -40,34 +44,39 @@ data "google_iam_policy" "metrics_publisher_subscriber" {
       "serviceAccount:${google_service_account.tenant_data_access.email}"
     ])
   }
+  count = var.enable_metrics_infra ? 1 : 0
 }
 
 resource "google_pubsub_topic_iam_policy" "metrics_publisher_subscriber_policy" {
-  project     = google_pubsub_topic.metrics_topic.project
-  topic       = google_pubsub_topic.metrics_topic.name
-  policy_data = data.google_iam_policy.metrics_publisher_subscriber.policy_data
+  project     = google_pubsub_topic.metrics_topic[0].project
+  topic       = google_pubsub_topic.metrics_topic[0].name
+  policy_data = data.google_iam_policy.metrics_publisher_subscriber[0].policy_data
+  count       = var.enable_metrics_infra ? 1 : 0
 }
 
 data "archive_file" "cloud_function_source_code" {
   type        = "zip"
   output_path = "publish_metrics.zip"
   source_dir  = "${path.module}/cloudfunction/publish_metrics/"
+  count       = var.enable_metrics_infra ? 1 : 0
 }
 
 resource "google_storage_bucket" "cloud_function_bucket" {
-  name                        = "${random_id.uuid.hex}-gcf-v2-source"
+  name                        = "${random_id.uuid[0].hex}-gcf-v2-source"
   location                    = var.storage_location
   uniform_bucket_level_access = true
+  count                       = var.enable_metrics_infra ? 1 : 0
 }
 
-resource "google_storage_bucket_object" "cloud_function_bucket" {
+resource "google_storage_bucket_object" "cloud_function_source" {
   name   = "publish_metrics.zip"
-  bucket = google_storage_bucket.cloud_function_bucket.name
-  source = data.archive_file.cloud_function_source_code.output_path
+  bucket = google_storage_bucket.cloud_function_bucket[0].name
+  source = data.archive_file.cloud_function_source_code[0].output_path
+  count  = var.enable_metrics_infra ? 1 : 0
 }
 
 resource "google_cloudfunctions2_function" "metric_publish_cloud_function" {
-  name        = "${random_id.uuid.hex}-publish-metrics"
+  name        = "${random_id.uuid[0].hex}-publish-metrics"
   location    = var.gcp_region
   description = "Publish Metrics to Control Plane"
 
@@ -76,8 +85,8 @@ resource "google_cloudfunctions2_function" "metric_publish_cloud_function" {
     entry_point = "publish_metrics"
     source {
       storage_source {
-        bucket = google_storage_bucket.cloud_function_bucket.name
-        object = google_storage_bucket_object.cloud_function_bucket.name
+        bucket = google_storage_bucket.cloud_function_bucket[0].name
+        object = google_storage_bucket_object.cloud_function_source[0].name
       }
     }
   }
@@ -89,7 +98,7 @@ resource "google_cloudfunctions2_function" "metric_publish_cloud_function" {
     timeout_seconds    = 60
     environment_variables = {
       TARGET_GOOGLE_CLOUD_PROJECT = var.control_plane_project
-      TARGET_TOPIC_NAME           = var.metrics_pubsub_topic
+      TARGET_TOPIC_NAME           = var.control_plane_metrics_pubsub_topic
     }
     ingress_settings      = "ALLOW_INTERNAL_ONLY"
     service_account_email = google_service_account.tenant_data_access.email
@@ -98,7 +107,7 @@ resource "google_cloudfunctions2_function" "metric_publish_cloud_function" {
   event_trigger {
     trigger_region = var.gcp_region
     event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
-    pubsub_topic   = google_pubsub_topic.metrics_topic.id
+    pubsub_topic   = google_pubsub_topic.metrics_topic[0].id
     retry_policy   = "RETRY_POLICY_RETRY"
   }
 
@@ -107,4 +116,6 @@ resource "google_cloudfunctions2_function" "metric_publish_cloud_function" {
     organisation-id   = lower(var.organisation_id)
     tenant-name       = lower(var.name)
   }
+
+  count = var.enable_metrics_infra ? 1 : 0
 }
