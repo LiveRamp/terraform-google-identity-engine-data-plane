b58072f543428ece58f91bd2a349d95fa514ebc9
kdodhi <kushil.dodhia@liveramp.com>
2024-04-29 13:14:04 +0100

    fix network name

--- network.tf
+++ network.tf
@@ -8,11 +8,12 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "dataproc_subnet" {
+  depends_on               = [google_compute_network.vpc_network]
   count                    = var.enable_dataproc_network ? 1 : 0
   project                  = var.data_plane_project
   region                   = var.gcp_region
   name                     = lower("${var.installation_name}-${var.country_code}-dataproc")
   ip_cidr_range            = var.dataproc_subnet_ip4_cidr
-  network                  = google_compute_network.vpc_network[*].id
+  network                  = google_compute_network.vpc_network[0].id
   private_ip_google_access = true
 }
