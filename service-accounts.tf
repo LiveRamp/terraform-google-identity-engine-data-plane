e541f1582d8f98e2aedea3fbd613cd4c6547270e
Burak Emre Kabakcı <emrekabakci@gmail.com>
2024-05-09 14:50:36 +0100

    Create HMAC keys for LiveRamp tenants (#39)

--- service-accounts.tf
+++ service-accounts.tf
@@ -7,3 +7,14 @@ resource "google_service_account" "tenant_data_access" {
   account_id  = lower("${var.name}-${random_id.generator.hex}")
   description = "Tenant data access serviceAccount for = ${var.installation_name} - ${title(var.name)} ${upper(var.country_code)}"
 }
+
+resource "google_service_account" "tenant_data_access" {
+  project     = var.data_plane_project
+  account_id  = lower("${var.name}-${random_id.generator.hex}")
+  description = "Tenant data access serviceAccount for = ${var.installation_name} - ${title(var.name)} ${upper(var.country_code)}"
+}
+
+resource "google_storage_hmac_key" "tenant_query_engine_access" {
+  count                 = var.enable_liveramp_query_engine ? 1 : 0
+  service_account_email = tenant_data_access.service_account.email
+}
