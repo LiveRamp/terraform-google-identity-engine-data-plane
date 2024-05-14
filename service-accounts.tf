2b676604eb80567c0fd11f111b3ffe2111d4cca0
Liutauras Vilda <liutauras.vilda@gmail.com>
2024-05-14 10:43:49 +0100

    PATCH/NO-JIRA - rm hmac keys (#43)

--- service-accounts.tf
+++ service-accounts.tf
@@ -7,8 +7,3 @@ resource "google_service_account" "tenant_data_access" {
   account_id  = lower("${var.name}-${random_id.generator.hex}")
   description = "Tenant data access serviceAccount for = ${var.installation_name} - ${title(var.name)} ${upper(var.country_code)}"
 }
-
-resource "google_storage_hmac_key" "tenant_query_engine_access" {
-  count                 = var.enable_liveramp_query_engine ? 1 : 0
-  service_account_email = google_service_account.tenant_data_access.email
-}
