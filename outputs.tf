c7b5b931ab2a809c0f5e624d7fdfdbfca0ac71c3
Paulo Almeida <paulo.almeida@liveramp.com>
2024-05-29 11:43:06 +0100

    PATCH/NO-JIRA add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,25 @@
-output "tenant_data_access_svc_account" {
-  value       = google_service_account.tenant_data_access
+output "tenant_organisation_id" {
+  value = var.organisation_id
+}
+
+output "tenant_name" {
+  value = var.name
+}
+
+output "tenant_project_id" {
+  value = data.google_project.data_plane_project.id
+}
+
+output "tenant_project_name" {
+  value = data.google_project.data_plane_project.name
+}
+
+output "tenant_project_number" {
+  value = data.google_project.data_plane_project.number
+}
+
+output "tenant_data_access_service_account_email" {
+  value       = google_service_account.tenant_data_access.email
   description = "The service account object that will be used to access the tenant data"
 }
 
@@ -23,6 +43,6 @@ output "output_bucket_name" {
   description = "The name of the GCS bucket that will be used to store the output files"
 }
 
-output "tenant_hmac_key" {
+output "tenant_hmac_key_id" {
   value = google_storage_hmac_key.this.id
 }
