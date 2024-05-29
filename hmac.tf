2a83798774d5ea33563db0babad6feb43fe3d433
Paulo Almeida <paulo.almeida@liveramp.com>
2024-05-29 10:27:08 +0100

    MINOR/IIGA2-1424 - provision data-plane HMAC key

--- hmac.tf
+++ hmac.tf
@@ -1,3 +1,3 @@
 resource "google_storage_hmac_key" "this" {
-  service_account_email = google_service_account.tenant_data_access.service_account.email
+  service_account_email = google_service_account.tenant_data_access.email
 }
