611d125c7f9b6a76aca3fe96c79b12cea28543aa
KDodhi <kushil.dodhia@liveramp.com>
2024-07-18 15:58:52 +0100

    MINOR/IIGA2-1466 Add `roles/logging.viewer` role for Dataproc logs (#57)
    * MINOR/IIGA2-1466 Add role for Dataproc logs

---
+++ dataproc_logs.tf
@@ -0,0 +1,31 @@
+
+locals {
+  groups_log_viewers = toset(concat(var.data_viewers.groups, var.data_editors.groups))
+  users_log_viewers  = toset(concat(var.data_viewers.users, var.data_editors.users))
+}
+
+resource "google_project_iam_member" "groups_log_viewers" {
+  for_each = local.groups_log_viewers
+  project  = var.data_plane_project
+  role     = "roles/logging.viewer"
+  member   = "group:${each.value}"
+
+  condition {
+    title       = "DataprocLogAccess"
+    description = "Access to logs from all Dataproc resources"
+    expression  = "resource.type == 'cloud_dataproc_job'"
+  }
+}
+
+resource "google_project_iam_member" "users_log_viewers" {
+  for_each = local.users_log_viewers
+  project  = var.data_plane_project
+  role     = "roles/logging.viewer"
+  member   = "user:${each.value}"
+
+  condition {
+    title       = "DataprocLogAccess"
+    description = "Access to logs from all Dataproc resources"
+    expression  = "resource.type == 'cloud_dataproc_job'"
+  }
+}
