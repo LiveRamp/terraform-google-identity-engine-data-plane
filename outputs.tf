ef0936678454535eff6ffb40899b66e4957c52f4
kdodhi <kushil.dodhia@liveramp.com>
2024-09-27 13:53:24 +0100

    default ip address to a string

--- outputs.tf
+++ outputs.tf
@@ -39,12 +39,12 @@ output "output_bucket_name" {
 }
 
 output "cloud_nat_static_ip_address_0" {
-  value       = try(google_compute_address.cloud_nat_static_ip_address[0].address, null)
+  value       = try(google_compute_address.cloud_nat_static_ip_address[0].address, "")
   description = "The static IP addresses for Cloud NAT"
 }
 
 output "cloud_nat_static_ip_address_1" {
-  value       = try(google_compute_address.cloud_nat_static_ip_address[1].address, null)
+  value       = try(google_compute_address.cloud_nat_static_ip_address[1].address, "")
   description = "The static IP addresses for Cloud NAT"
 }
 
