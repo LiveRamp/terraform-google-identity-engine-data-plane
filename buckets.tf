609f6d3d2939010b42c496626c6753433f959213
Tolu Onasanya <tolu.onasanya@liveramp.com>
2025-01-17 11:31:15 +0000

    remove kms from gcs storage

--- buckets.tf
+++ buckets.tf
@@ -1,22 +1,14 @@
 locals {
-  default_build_bucket_name  = lower("${var.installation_name}-${var.environment}-${var.name}-${var.country_code}-build")
-  build_bucket_name          = coalesce(var.build_bucket_name, local.default_build_bucket_name)
+  default_build_bucket_name = lower("${var.installation_name}-${var.environment}-${var.name}-${var.country_code}-build")
+  build_bucket_name         = coalesce(var.build_bucket_name, local.default_build_bucket_name)
 }
 
 resource "google_storage_bucket" "tenant_build_bucket" {
   provider                    = google-beta
-  depends_on                  = [google_kms_crypto_key.tenant_crypto_key, module.kms_crypto_key-iam-bindings]
   project                     = var.data_plane_project
   name                        = local.build_bucket_name
   location                    = var.storage_location
   uniform_bucket_level_access = true
-
-  dynamic "encryption" {
-    for_each = var.enable_kms ? [google_kms_crypto_key.tenant_crypto_key[0].id] : []
-    content {
-      default_kms_key_name = google_kms_crypto_key.tenant_crypto_key[0].id
-    }
-  }
 
   labels = {
     organisation-id = lower(var.organisation_id)
