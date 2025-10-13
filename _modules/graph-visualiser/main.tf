78408780c0f790f5f81529c586e2e10232b8c67e
Paulo Almeida <paulo.almeida@liveramp.com>
2025-10-13 15:03:56 +0100

    test - network permission

--- _modules/graph-visualiser/main.tf
+++ _modules/graph-visualiser/main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_v2_service" "graph_visualiser" {
       }
     }
     scaling {
-      max_instance_count = 1
+      max_instance_count = 2
       min_instance_count = 0
     }
     service_account = var.service_account
